6c436d379d0c80d1e487ba6565fa112d28f67535
ph3l1x <rperry@mindimage.net>
2015-12-22 01:12:25 -0700

    Watch Stuff

--- js/main.js
+++ js/main.js
@@ -1,32 +1,38 @@
        var battery = navigator.battery || navigator.mozBattery || navigator.webkitBattery;
 
         window.addEventListener('load', function (e) {
-            document.querySelector('#charging').textContent = battery.charging ? 'charging' : 'not charging';
-            document.querySelector('#chargingTime').textContent = battery.chargingTime / 60;
-            document.querySelector('#dischargingTime').textContent = battery.dischargingTime / 60;
-            document.querySelector('#level').textContent = battery.level;
-        }, false);
-
-        battery.addEventListener('chargingchange', function () {
-            // battery.charging : true or false
-            document.querySelector('#charging').textContent = battery.charging ? 'charging' : 'not charging';
-        }, false);
-
-        battery.addEventListener('chargingtimechange', function () {
-            // battery.chargingTime : second(ex: 3600) or Infinity
-            document.querySelector('#chargingTime').textContent = battery.chargingTime;// second(ex: 3600) or Infinity
-        }, false);
-
-        battery.addEventListener('dischargingtimechange', function () {
-            // battery.dischargingTime : second(ex: 3600) or Infinity
-            document.querySelector('#dischargingTime').textContent = battery.dischargingTime;
+            document.querySelector('.battery-text').textContent = battery.level;
         }, false);
 
         battery.addEventListener('levelchange', function () {
-            // battery.level : between 0 and 1 (ex: 0.50)
-            document.querySelector('#level').textContent = Math.floor(battery.level * 100) + '%';// 50%
-
-            if (battery.level < 0.05) {
-                alert('battery level below 5%!');
+        	var batLevel = Math.floor(battery.level * 100);
+            var textSelector = document.querySelector('.battery-text');
+            var leftImage = document.querySelector('.battery-image-left');
+            var rightImage = document.querySelector('.battery-image-right');
+            var centerImage = document.querySelector('.battery-image-center');
+        	
+   
+            
+            if(batLevel > 70) {
+                centerImage.style.width = batLevel * 1.52 + 'px';
+                rightImage.style.left = batLevel * 1.52 + 18 + 'px';
+            } else if(batLevel > 50)  {
+            	centerImage.style.width = batLevel * 1.75 + 5 + 'px';
+                rightImage.style.left = batLevel * 1.75 + 23 + 'px';
+            } else {
+            	centerImage.style.width = batLevel * 1.75 + 'px';
+                rightImage.style.left = batLevel * 1.75 + 18 + 'px';	
             }
+            
+            if (batLevel < 3) {
+            	leftImage.style.left = '18px';
+            	rightImage.style.display = 'none';
+            	centerImage.style.display = 'none';
+            } else {
+            	leftImage.style.display = "block";
+            	rightImage.style.display = "block";
+            	centerImage.style.display = "block";
+            }
+            textSelector.textContent = batLevel + '%';// 50%
+        	console.log(batLevel);
         }, false);
