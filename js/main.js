ababdd5e7401ba3f67f97ef640b75b69e979897c
ph3l1x <rperry@mindimage.net>
2015-12-22 18:13:00 -0700

    Polishing touches

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
        var battery = navigator.battery || navigator.mozBattery || navigator.webkitBattery;
 
 
-        window.addEventListener('load', function (e) {
-            document.querySelector('.battery-text').textContent = battery.level;
+        window.addEventListener('load', function () {
+            document.querySelector('.battery-text').textContent = Math.floor(battery.level * 100);
         }, false);
 
         battery.addEventListener('levelchange', function () {
-        	var batLevel = Math.floor(battery.level * 100);
-            var textSelector = document.querySelector('.battery-text');
-            var leftImage = document.querySelector('.battery-image-left');
-            var rightImage = document.querySelector('.battery-image-right');
-            var centerImage = document.querySelector('.battery-image-center');
-        	
+        	var batLevel = Math.floor(battery.level * 100),
+            	textSelector = document.querySelector('.battery-text'),
+            	rightImage = document.querySelector('.battery-image-right'),
+            	centerImage = document.querySelector('.battery-image-center');
    
             centerImage.style.width = 152 * (batLevel/100) + 'px';
             rightImage.style.left = 152 * (batLevel/100) + 18 + 'px';
@@ -23,5 +21,5 @@
             	rightImage.style.display = "block";
             	centerImage.style.display = "block";
             }
-            textSelector.textContent = batLevel + '%';// 50%
+            textSelector.textContent = batLevel + '%';
         }, false);
